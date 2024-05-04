7b0e8cb63d8a145480a5d5a507c84cf4520275c6
axseem <tr1xn.gg@gmail.com>
2024-05-04 16:52:35 +0200

    feat: zig solve day01 part2

--- zig/2023/01/main.zig
+++ zig/2023/01/main.zig
@@ -7,17 +7,67 @@ pub fn part1(input: []const u8) usize {
     while (lines.next()) |line| {
         var i: usize = 0;
         while (i < line.len) : (i += 1) {
-            if (line[i] >= '1' and line[i] <= '9') {
-                sum += (line[i] - 48) * 10;
+            const c: u8 = line[i];
+            if (c >= '1' and c <= '9') {
+                sum += (c - 48) * 10;
                 break;
             }
         }
 
         i = line.len - 1;
         while (i >= 0) : (i -= 1) {
-            if (line[i] >= '1' and line[i] <= '9') {
-                sum += (line[i] - 48);
+            const c: u8 = line[i];
+            if (c >= '1' and c <= '9') {
+                sum += (c - 48);
                 break;
+            }
+        }
+    }
+
+    return sum;
+}
+
+pub fn part2(input: []const u8) usize {
+    var lines = std.mem.split(u8, input, "\n");
+    const digits = [_][]const u8{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+    var sum: usize = 0;
+    while (lines.next()) |line| {
+        var i: usize = 0;
+        first: while (i < line.len) : (i += 1) {
+            const c: u8 = line[i];
+            if (c >= '1' and c <= '9') {
+                sum += (c - 48) * 10;
+                break;
+            }
+
+            for (digits, 0..) |digit, j| {
+                var k: usize = 0;
+                while (line[i + k] == digit[k]) : (k += 1) {
+                    if (digit.len - 1 == k) {
+                        sum += (j + 1) * 10;
+                        break :first;
+                    }
+                }
+            }
+        }
+
+        i = line.len - 1;
+        last: while (i >= 0) : (i -= 1) {
+            const c: u8 = line[i];
+            if (c >= '1' and c <= '9') {
+                sum += (c - 48);
+                break;
+            }
+
+            for (digits, 0..) |digit, j| {
+                var k: usize = 0;
+                while (line[i - k] == digit[digit.len - 1 - k]) : (k += 1) {
+                    if (digit.len - 1 == k) {
+                        sum += j + 1;
+                        break :last;
+                    }
+                }
             }
         }
     }
@@ -28,5 +78,7 @@ pub fn part1(input: []const u8) usize {
 pub fn main() void {
     const input = @embedFile("./input.txt");
 
-    std.debug.print("{d}", .{part1(input)});
+    std.debug.print("--- 2023 day 01 answer ---\n", .{});
+    std.debug.print("part 1:\t{d}\n", .{part1(input)});
+    std.debug.print("part 2:\t{d}\n", .{part2(input)});
 }
